c7a71c95a68f6bab6ec23de15671e5670ac26fdf
tforce7171 <68318576+tforce7171@users.noreply.github.com>
2021-06-30 11:56:13 +0900

    Add files via upload

--- test0.py
+++ test0.py
@@ -1,3 +1,9 @@
 import subprocess
 
-subprocess.Popen(["python","./test1.py"])
+i = 0
+while True:
+    if i == 0:
+        subprocess.Popen(["python","./test1.py"])
+        subprocess.Popen(["python","./test2.py"])
+        subprocess.Popen(["python","./test3.py"])
+    i = 1
